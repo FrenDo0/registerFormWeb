a9909a43d36885f10d3af7bc90ef49e16021bfd2
FrenDo0 <74112298+FrenDo0@users.noreply.github.com>
2023-03-10 11:28:06 +0200

    Add files via upload
    Added settings for incorrect attempts to log in + user lock out account

--- LogIn.cshtml.cs
+++ LogIn.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySqlX.XDevAPI;
 using System.Web.Helpers;
+using System.Data;
 
 namespace testWeb.Pages
 {
@@ -44,22 +45,38 @@ namespace testWeb.Pages
             String password = Request.Form["password"];
             String hashPass = "";
             bool correctPass = false;
-            bool checker = false;
+            bool checkUserExisting = false;
             bool checkUser = checkUsername(username);
-            if(checkUser == true)
+
+            Int64 userId = getUserId(username);
+            DateTime currentDate = new DateTime();
+            currentDate = DateTime.Now;
+            bool checkAttempt = checkIfUserIsExistingInIncorrectAttempts(userId);
+            int maxAttempts = numberOfAttemptsSettings();
+
+
+            DateTime lastTime = getTime(userId);
+            TimeSpan timePassed = currentDate - lastTime;
+            Int64 rowId = getRowId(userId);
+            DateTime userLockedOut = getUserLockOutTime(userId);
+            DateTime releaseTimeOut = DateTime.Now;
+            bool isUserLocked = userLockedOut > releaseTimeOut;
+
+            if (checkUser == true)
             {
                 hashPass = getPassByUsername(username);
                 correctPass = Crypto.VerifyHashedPassword(hashPass,password);
                 if(correctPass == true)
                 {
-                    checker = isExisting(username,hashPass);
+                    checkUserExisting = isExisting(username,hashPass);
                 }
+                
             }
             else
             {
                 errorMsg = "Invalid username !";
             }
-            if (checkUser == true && checker == true && correctPass == true)
+            if (checkUser == true && checkUserExisting == true && correctPass == true)
             {
                 accountDeleted = isActive(username);
                 if (accountDeleted.Equals("TRUE"))
@@ -67,8 +84,15 @@ namespace testWeb.Pages
                     confirmed = isConfirmed(username);
                     if (confirmed.Equals("TRUE"))
                     {
-                        HttpContext.Session.SetString("sessionUsername", username);
-                        Response.Redirect("/HomePage");
+                        if(checkAttempt == true && isUserLocked == true)
+                        {
+                            errorMsg = "Your account has been locked until " + userLockedOut + " Please try again later";
+                        }
+                        else
+                        {
+                            HttpContext.Session.SetString("sessionUsername", username);
+                            Response.Redirect("/HomePage");
+                        }
                     }
                     else
                     {
@@ -82,10 +106,47 @@ namespace testWeb.Pages
                     errorMsg = "This username is not existing";
                 }
             }
-            else if(checkUser == true && checker == false)
+            else if(checkUser == true && checkUserExisting == false)
             {
                 numberAttempts++;
-                errorMsg = "Incorrect password. Please try again." + numberAttempts;
+                errorMsg = "Incorrect password. Please try again.";
+               
+
+                if(checkAttempt == false)
+                {
+                    incorrectAttempt(userId, numberAttempts, currentDate);
+                }
+                else
+                {
+                    if (!isUserLocked)
+                    {
+                        if (checkAttempt == true && timePassed.TotalMinutes <= 3)
+                        {
+                            int numberOfAttempts = checkAttempts(rowId);
+                            if (numberOfAttempts < maxAttempts)
+                            {
+                                numberOfAttempts++;
+                                updateIncorrectAttempt(rowId, numberOfAttempts, currentDate);
+                            }
+                            else
+                            {
+                                test = "You have tried so many times with wrong passowrd. Now you are locked out";
+                                int seconds = userLockOut();
+                                DateTime lockOutTo = currentDate.AddSeconds(seconds);
+                                lockOutTime(rowId, lockOutTo);
+                            }
+
+                        }
+                        else
+                        {
+                            incorrectAttempt(userId, numberAttempts, currentDate);
+                        }
+                    }
+                    else
+                    {
+                        test = "You are locked out until " + userLockedOut;
+                    }
+                }                
             }
             else
             {
@@ -94,6 +155,70 @@ namespace testWeb.Pages
             
         }
 
+      
+        public DateTime getUserLockOutTime(Int64 userId)
+        {
+            DateTime time = new DateTime();
+            
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT TOP 1 timeout_to FROM incorrectAttempts WHERE user_id=@userId ORDER BY Id DESC";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("userId", userId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                               time = reader.GetDateTime(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace + " " + ex.Message;
+            }
+            return time;
+        }
+        public bool checkIfUserIsExistingInIncorrectAttempts(Int64 userId)
+        {
+            bool check = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT COUNT(*) FROM incorrectAttempts WHERE user_id=@userId";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@userId", userId);
+                        Int32 count = (Int32)cmd.ExecuteScalar();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                if (count > 0)
+                                {
+                                    check = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace + " " + ex.Message;
+            }
+            return check;
+        }
         public String getPassByUsername(String username)
         {
             String pass = "";
@@ -123,19 +248,20 @@ namespace testWeb.Pages
             }
             return pass;
         }
-        public void updateIncorrectAttempt(Int64 userId, int attempt)
+        public void updateIncorrectAttempt(Int64 rowId, int attempt,DateTime time)
         {
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "UPDATE incorrectAttempts SET number_attempts=@attempts WHERE user_id=@id";
+                    String sql = "UPDATE incorrectAttempts SET number_attempts=@attempts,time=@time WHERE Id=@id";
 
                     using (SqlCommand cmd = new SqlCommand(sql, connection))
                     {
                         cmd.Parameters.AddWithValue("@attempts", attempt);
-                        cmd.Parameters.AddWithValue("@id", userId);
+                        cmd.Parameters.AddWithValue("@time",time);
+                        cmd.Parameters.AddWithValue("@id", rowId);
                         cmd.ExecuteNonQuery();
                     }
                 }
@@ -145,27 +271,132 @@ namespace testWeb.Pages
                 errorMsg = "asd" + ex.StackTrace;
             }
         }
+        public Int64 getRowId(Int64 userId)
+        {
+            Int64 rowId = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT TOP 1 Id FROM incorrectAttempts WHERE user_id=@userId ORDER BY Id DESC ";
 
-        //public int checkAttempts()
-        //{
-        //    int attempts = 0;
-        //    return attempts;
-        //}
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("userId", userId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                rowId = reader.GetInt64(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace;
+            }
+            return rowId;
+        }
+        public DateTime getTime(Int64 userId)
+        {
+            DateTime time = new DateTime();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT TOP 1 time FROM incorrectAttempts WHERE user_id=@userId ORDER BY Id DESC ";
 
-        public void incorrectAttempt(Int64 userId,int attempts,DateTime date)
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("userId", userId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                time = reader.GetDateTime(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace;
+            }
+            return time;
+        }
+        public void lockOutTime(Int64 rowId, DateTime time)
         {
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "INSERT INTO incorrectAttempts (user_id,number_attempts,time) " +
-                        "VALUES (@userId,@numberAttempts,@time)";
+                    String sql = "UPDATE incorrectAttempts SET timeout_to=@timeout WHERE Id=@id";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@timeout", time);
+                        cmd.Parameters.AddWithValue("@id", rowId);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = "asd" + ex.StackTrace + " " + ex.Message;
+            }
+        }
+        public int checkAttempts(Int64 rowId)
+        {
+            int attempts = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT number_attempts FROM incorrectAttempts WHERE Id=@rowId";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("rowId", rowId);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                attempts = reader.GetInt32(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace;
+            }
+            return attempts;
+        }
+
+        public void incorrectAttempt(Int64 userId,int attempts,DateTime currentDate)
+        {
+            DateTime defaultDate = DateTime.Now;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO incorrectAttempts (user_id,number_attempts,time,timeout_to) " +
+                        "VALUES (@userId,@numberAttempts,@time,@timeout)";
                     using (SqlCommand cmd = new SqlCommand(sql, connection))
                     {
                         cmd.Parameters.AddWithValue("@userId", userId);
                         cmd.Parameters.AddWithValue("numberAttempts", attempts);
-                        cmd.Parameters.AddWithValue("time",date);
+                        cmd.Parameters.AddWithValue("time",currentDate);
+                        cmd.Parameters.AddWithValue("@timeout",defaultDate);
                         
                         cmd.ExecuteNonQuery();
                     }
@@ -173,7 +404,7 @@ namespace testWeb.Pages
             }
             catch (Exception ex)
             {
-                errorMsg = "Already existing username or email !";
+                errorMsg = ex.StackTrace;
             }
         }
         public int userLockOut()
@@ -204,7 +435,7 @@ namespace testWeb.Pages
             }
             return time;
         }
-        public int numberOfAttempts()
+        public int numberOfAttemptsSettings()
         {
             int number = 0;
             try
@@ -232,6 +463,36 @@ namespace testWeb.Pages
             }
             return number;
         }
+
+        public Int64 getUserId(String username)
+        {
+            Int64 userId = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT user_id FROM users WHERE user_username=@username";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("username",username);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                userId = reader.GetInt64(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.StackTrace;
+            }
+            return userId;
+        }
         public void changeUserStatus(String userId)
         {
             String status = "TRUE";
@@ -255,13 +516,6 @@ namespace testWeb.Pages
             {
                 errorMsg = "asd" + ex.StackTrace;
             }
-        }
-
-        public string encryptPassword(string pass)
-        {
-            byte[] storePass = ASCIIEncoding.ASCII.GetBytes(pass);
-            string encryptedPass = Convert.ToBase64String(storePass);
-            return encryptedPass;
         }
        
         public string isActive(String username)
